86937623680c038e499ac8b966bee4923a106898
web-coder-omit <92245674+web-coder-omit@users.noreply.github.com>
2024-04-03 03:59:20 +0600

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,17 @@
+let header_child_02 = document.querySelector('.header_child_02');
+let header_child_03 = document.querySelector('.header_child_03');
+let x = document.querySelector('.x');
+let nav = document.querySelector('.nav');
+
+function toggle(){
+    header_child_02.style.display = 'flex';
+    header_child_03.style.display = 'flex';
+    console.log('hi')
+
+
+};
+function closes(){
+    header_child_02.style.display = 'none';
+    header_child_03.style.display = 'none';
+}
+
